7a57878cb7d969feb5fe95ec5fe7e90f2cb2f884
Eugene Lisitsky <lisitsky@gmail.com>
2018-09-08 23:56:09 +0300

    Fix compile
    New changes to method were introduced to fix compilation

--- src/main/scala/lectures/types/FixCompile.scala
+++ src/main/scala/lectures/types/FixCompile.scala
@@ -8,14 +8,19 @@ package lectures.types
   * * Раскоментируйте код
   * * Исправьте тело метода mapper так, чтобы программа скомпилировалась
   */
+
 object FixCompile extends App {
 
-//  val mapper = (i: String) => if (i % 2 != 0) i * 2
-//
-//  val result = List(1, 2, 3, 4, 5, 6, 7, 8, 9).map {
-//      mapper
-//    }.foldLeft(0) { (acc, v) => acc + v }
-//
-//   print(result)
+//  override def main(args: Array[String]): Unit = {
+
+
+    val mapper = (i: Int) => if (i % 2 != 0) i * 2 else 0
+
+    val result = List(1, 2, 3, 4, 5, 6, 7, 8, 9).map {
+      mapper
+    }.foldLeft(0) { (acc, v) => acc + v }
+
+    print(result)
+//  }
 }
 
